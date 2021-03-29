482dd7a94e69045f61878ed9734cd253115fbc11
truckershitch <truckershitch@hambone.e4ward.com>
2021-03-29 11:01:20 -0400

    20210329 pre-lunch fixes

--- pyscript/update_options.py
+++ pyscript/update_options.py
@@ -1,15 +1,14 @@
 
 @service
 def update_options(entity_id, options_csv, retain_state='hell no'):
-    retain_state = 'hell yes' in retain_state.lower()
     if entity_id is not None and options_csv is not None:
         options = [ o.strip() for o in options_csv.split(",") ]
 
         if entity_id.startswith('input_select') and entity_id.count('.') == 1:
             input_select.set_options(entity_id=entity_id, options=options)
 
-            if retain_state:
-                current_state = entity_id
+            if retain_state == 'hell yes':
+                current_state = state.get(entity_id)
 
                 if current_state is not None and current_state in options:
                     input_select.select_option(entity_id=entity_id, option=current_state)
